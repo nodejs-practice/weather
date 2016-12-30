22961a0764f4bf4c628f9e4bceffff0e8f86cc8d
Castor Troy <castort+nodejspractice@outlook.com>
2016-12-30 13:53:09 +0530

    Added php folder to test php stuff

---
+++ php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <style>
+        body {padding:0;}
+        header {background-color: grey;}
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Hello World</h1>
+        <?php
+	$var = 'xyz';
+        	echo 'Welcome ' . $_GET['name'] ;
+        ?>
+    </header>
+</body>
+</html>
